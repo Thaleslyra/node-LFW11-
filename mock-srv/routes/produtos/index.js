704008d3a26f65128d52c7d545586efcf50535e2
THALES ANDRÉ DA SILVA LYRA <thalesalyra@gmail.com>
2024-10-10 17:30:23 -0300

    fastfy
    mock data with fastfy

---
+++ mock-srv/routes/produtos/index.js
@@ -0,0 +1,9 @@
+"use strict"
+
+export default async function (fastify) {
+    {
+        fastify.get("/", async function (request, reply) {
+            return "Hello, World!";
+        })
+    }
+}
